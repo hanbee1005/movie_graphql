954839c1c1b5e03c5db2720d304a5f93957eb7b5
hanbee1005 <sonhanbi1002@gmail.com>
2021-04-12 15:03:51 +0900

    movies 데이터로 변경 및 함수로 데이터에 접근하도록 수정

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,42 @@
-export const people = [
+let movies = [
     {
         id: 0,
-        name: "Nicolas",
-        age: 18,
-        gender: "female"
+        name: "Star Wars - The new one",
+        score: 1
     },
     {
         id: 1,
-        name: "Alice",
-        age: 20,
-        gender: "female"
+        name: "Avengers - The new one",
+        score: 10
     },
     {
         id: 2,
-        name: "Bob",
-        age: 30,
-        gender: "male"
+        name: "The Godfather I",
+        score: 9
     },
     {
         id: 3,
-        name: "JD",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: 4,
-        name: "moondaddi",
-        age: 25,
-        gender: "male"
-    },
-    {
-        id: 5,
-        name: "flym",
-        age: 36,
-        gender: "female"
+        name: "Logan",
+        score: 4
     }
 ];
 
+// 전체 영화 조회하기
+export const getMovies = () => movies;
+
+// id 로 영화 조회하기
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id);
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+}
+
+// 해당 id 영화 지우기
+export const deleteMovie = id => {
+    const cleanMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanMovies.length) {
+        movies = cleanMovies;
+        return true;
+    } else {
+        return false;
+    }
 }
